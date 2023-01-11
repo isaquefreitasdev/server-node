06e675a1869221bb78e28c0225321bf086ae08de
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-01-11 12:40:02 -0300

    ser

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-let http = require("http")
+const http = require("http")
+const url = require('url')
+const fs = require('fs')
 
 
+http.createServer((req, res) => {
+    let path = url.parse(req.url).pathname;
+    if(path == '' || path == "/"){
+        path = '/index.html'
+    }
+    let fileName = "." + path
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content': 'text/plain'})
-    // Conteudo
-    res.end('Hello,World')
-}).listen(3000,(err)=>{
-    if(err){
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end("<h1>ERROR 404!NENHUMA PÁGINA ENCONTRADA!")
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" })
+            res.write(data)
+            res.end()
+        }
+    })
+}).listen(3000, (err) => {
+    if (err) {
         console.log(err)
-    }else{
+    } else {
         console.log('servidor rodando')
     }
 })
